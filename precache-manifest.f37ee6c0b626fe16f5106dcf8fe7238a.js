497cdfc612cf9433565db65f0051f34c25afc523
luron <joseluron@gmail.com>
2020-03-02 00:47:45 +0100

    Updates

---
+++ precache-manifest.f37ee6c0b626fe16f5106dcf8fe7238a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec585feb36ed8a9eee875af75d98c9a",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "8944130a304c38d7b391",
+    "url": "/my-resume/static/css/main.ad9cb3ad.chunk.css"
+  },
+  {
+    "revision": "6776f8c91b6e3cec5cca",
+    "url": "/my-resume/static/js/2.615a32ea.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-resume/static/js/2.615a32ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8944130a304c38d7b391",
+    "url": "/my-resume/static/js/main.5ff06d5b.chunk.js"
+  },
+  {
+    "revision": "ae83ed400195c95e6b37",
+    "url": "/my-resume/static/js/runtime-main.7db97161.js"
+  },
+  {
+    "revision": "058a5b6f0e4bed8f73f20c466c1f705d",
+    "url": "/my-resume/static/media/githubIcon.058a5b6f.svg"
+  },
+  {
+    "revision": "4d349cc1a25f4db9c8140a3f4b594242",
+    "url": "/my-resume/static/media/linkdinIcon.4d349cc1.svg"
+  },
+  {
+    "revision": "8fdf734a92a6ab6b99d8bc31b6fd8abc",
+    "url": "/my-resume/static/media/locationIcon.8fdf734a.svg"
+  }
+]);
