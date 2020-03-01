ceadb229fbd045293b9d3a4b9ca9cc54c2022573
luron <joseluron@gmail.com>
2020-03-01 15:01:00 +0100

    Added "Skills", "Languages" and "Hobbies" sections

--- src/components/LeftSideColumn/LeftSideColumn.js
+++ src/components/LeftSideColumn/LeftSideColumn.js
@@ -32,6 +32,60 @@ function LeftSideColumn() {
             href: "https://www.linkedin.com/in/jos%C3%A9-luis-coalla-cencerrado-797447138/"
         }
     ];
+    const skillsData = [
+        {
+            skill: "Angular 2+",
+            value: 5
+        }, 
+        {
+            skill: "Ionic",
+            value: 5
+        },
+        {
+            skill: "ReactJS",
+            value: 5
+        },
+        {
+            skill: "NodeJS",
+            value: 4
+        },
+        {
+            skill: "Git",
+            value: 4
+        },
+        {
+            skill: "REST",
+            value: 3
+        }
+    ];
+    const languagesData = [
+        {
+            language: "Spanish",
+            value: 5
+        },
+        {
+            language: "English",
+            value: 4
+        }
+    ];
+
+    const renderDotValue = value => {
+        const valueToRender = [];
+        for (let i = 1; i <= value; i++) {
+            valueToRender.push(<div className="value" />);
+        }
+
+        return valueToRender;
+    }
+
+    const renderDotDummy = value => {
+        const dummyToRender = [];
+        for (let i = 1; i <= value; i++) {
+            dummyToRender.push(<div className="dummy-value" />);
+        }
+
+        return dummyToRender;
+    }
     
     return (
         <div className="left-side-column-container">
@@ -60,6 +114,38 @@ function LeftSideColumn() {
                             </div>
                         );
                     })}
+                </div>
+                <div className="skills-wrapper">
+                    <span className="header">Skills</span>
+                    {skillsData.map(skill => {
+                        return (
+                            <div className="skill-block">
+                                <span className="skill">{skill.skill}</span>
+                                <div className="value-wrapper">
+                                    {renderDotValue(skill.value)}
+                                    {renderDotDummy(5 - skill.value)}
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="languages-wrapper">
+                    <span className="header">Languages</span>
+                    {languagesData.map(language => {
+                        return (
+                            <div className="language-block">
+                                <span className="language">{language.language}</span>
+                                <div className="value-wrapper">
+                                    {renderDotValue(language.value)}
+                                    {renderDotDummy(5 - language.value)}
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="hobbies-wrapper">
+                    <span className="header">Hobbies</span>
+                    <span className="hobbies">Going to the movies, gym, going out, eating, listening to music, traveling, hiking, learning</span>
                 </div>
             </div>
         </div>
